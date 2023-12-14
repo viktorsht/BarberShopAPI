17f3afd9e78b8fb31372a1dd1658c09db3ddd1e0
mr-santos <mr-santos@mr-santos.mr-santos>
2023-12-14 14:15:54 -0300

    Criando get e update de horários

--- src/main/java/com/api/barber/app/hoursActive/sevices/HoursActiveService.java
+++ src/main/java/com/api/barber/app/hoursActive/sevices/HoursActiveService.java
@@ -1,4 +1,33 @@
 package com.api.barber.app.hoursActive.sevices;
 
+import com.api.barber.app.hoursActive.dto.HoursActiveDTO;
+import com.api.barber.app.hoursActive.entity.HoursActiveEntity;
+import com.api.barber.app.hoursActive.repositoty.HoursActiveRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class HoursActiveService {
+    final private HoursActiveRepository hoursActiveRepository;
+
+    public HoursActiveService(HoursActiveRepository hoursActiveRepository) {
+        this.hoursActiveRepository = hoursActiveRepository;
+    }
+
+    public List<HoursActiveEntity> listHoursActive(){
+        return hoursActiveRepository.findAll();
+    }
+
+    public void updateHoursActive(int hoursActiveId, HoursActiveDTO hoursActiveDTO){
+        var hoursEntity = hoursActiveRepository.findById(hoursActiveId);
+        if(hoursEntity.isPresent()) {
+            var hours = hoursEntity.get();
+            if(hoursActiveDTO.status() != null){
+                hours.setStatus(hoursActiveDTO.status());
+                hoursActiveRepository.save(hours);
+            }
+        }
+    }
+
 }
