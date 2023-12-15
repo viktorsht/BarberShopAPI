b82de94f01f5648cc679af7ce38db21d970e4130
mr-santos <mr-santos@mr-santos.mr-santos>
2023-12-15 15:30:19 -0300

    Criando service dos métodos de pagamento

---
+++ src/main/java/com/api/barber/app/paymentMethods/services/PaymentMethodsService.java
@@ -0,0 +1,35 @@
+package com.api.barber.app.paymentMethods.services;
+
+import com.api.barber.app.paymentMethods.dto.PaymentMethodsDTO;
+import com.api.barber.app.paymentMethods.entity.PaymentMethodsEntity;
+import com.api.barber.app.paymentMethods.repository.PaymentMethodsRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PaymentMethodsService {
+    final private PaymentMethodsRepository paymentMethodsRepository;
+
+    public PaymentMethodsService(PaymentMethodsRepository paymentMethodsRepository) {
+        this.paymentMethodsRepository = paymentMethodsRepository;
+    }
+
+    public List<PaymentMethodsEntity> listPaymentMethods(){
+        return paymentMethodsRepository.findAll();
+    }
+
+    public void updatePaymentMethods(int paymentMethodsId, PaymentMethodsDTO paymentMethodsDTO){
+        var paymentMethodsEntity = paymentMethodsRepository.findById(paymentMethodsId);
+        if(paymentMethodsEntity.isPresent()){
+            var payment = paymentMethodsEntity.get();
+            if(paymentMethodsDTO.name() != null){
+                payment.setName(paymentMethodsDTO.name());
+            }
+            if(paymentMethodsDTO.status() != null){
+                payment.setStatus(paymentMethodsDTO.status());
+            }
+            paymentMethodsRepository.save(payment);
+        }
+    }
+}
